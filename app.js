0425336d5036899d9e03101cca7a2402027ea5f9
freeruntrip <official_zeomcompany@naver.com>
2026-05-19 23:08:07 +0900

    Fix Vercel app.js sync

--- app.js
+++ app.js
@@ -8,15 +8,32 @@ const startBtn = document.getElementById('startBtn');
 startBtn.addEventListener('click', function () {
   console.log('러닝 시작 버튼 클릭됨');
 
-  navigator.geolocation.getCurrentPosition(
-    function (position) {
-      console.log('GPS 성공');
-      console.log(position);
-    },
-    function (error) {
-      console.log('GPS 에러 발생');
-      console.log(error);
-    }
-  );
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    console.log('GPS 성공');
+    console.log(position);
+
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+
+    console.log(latitude, longitude);
+
+    map.setView([latitude, longitude], 16);
+
+    L.marker([latitude, longitude]).addTo(map)
+      .bindPopup('현재 위치')
+      .openPopup();
+  },
+
+  function (error) {
+    console.log('GPS 에러 발생');
+    console.log(error);
+  },
+
+  {
+    enableHighAccuracy: true
+  }
+);
 });
-console.log('파일 저장 테스트');
+console.log('VERCEL TEST SUCCESS');
+
